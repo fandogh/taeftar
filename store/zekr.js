9c2d43883292a4ba05501c590d928cd05c833ae9
Pooya Parsa <pooya@pi0.ir>
2018-05-17 10:38:59 +0430

    update zekr.js

--- store/zekr.js
+++ store/zekr.js
@@ -2,7 +2,7 @@ import zekr from '../lib/zekr';
 
 export default {
   state() {
-    const today = String(Math.ceil(Math.abs(Date.now() - new Date('5/27/2017')) / (1000 * 3600 * 24)));
+    const today = String(Math.ceil(Math.abs(Date.now() - new Date('5/17/2018')) / (1000 * 3600 * 24)));
     return zekr[today];
   }
 }
