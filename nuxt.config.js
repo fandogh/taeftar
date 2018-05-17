830670050fb32452297e3d573a35ff0382623b9b
Pooya Parsa <pooya@pi0.ir>
2018-05-17 10:34:25 +0430

    feat: upgrade to nuxt 2

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,30 +1,31 @@
-module.exports = {
-  loading: {color: '#E39657'},
-  head: {
-    title: 'تا افطار',
-    meta: [
-      {name: 'viewport', content: 'width=device-width, initial-scale=1'}
-    ]
+export default {
+  mode: 'spa',
+
+  loading: {
+    color: '#E39657'
   },
+
+  manifest: {
+    name: 'تا افطار',
+    lang: 'fa',
+    version: '2.0.0',
+    short_name: 'تا افطار',
+    description: 'زمان شمار افطار',
+    theme_color: '#E39657'
+  },
+
   css: [
     '~assets/css/circle.css',
     '~assets/css/main.css'
   ],
+
   build: {
     extractCSS: true
   },
-  ssr: false,
-  modules: [
-    require('@nuxtjs/axios'),
-    require('@nuxtjs/manifest'),
-    require('@nuxtjs/offline'),
-    // [require('@nuxtjs/localtunnel'), {subdomain: 'taeftar'}],
-    {
-      src: require('@nuxtjs/google-analytics'),
-      options: {
-        ua: 'UA-79535343-1'
-      }
-    }
-  ]
 
+  modules: [
+    '@nuxtjs/axios',
+    '@nuxtjs/pwa',
+    ['@nuxtjs/google-analytics', { ua: 'UA-79535343-1' }]
+  ]
 }
